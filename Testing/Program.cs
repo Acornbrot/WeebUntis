da1db01fbc9e912d94042677245763793b31a79f
Linus <linus.schneeberg@gmail.com>
2026-01-23 23:17:40 +0100

    Implement fetchIdentity

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,4 +1,17 @@
-﻿using UntisAPI;
+﻿using Microsoft.Extensions.Configuration;
+using UntisAPI;
+
+IConfiguration config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddJsonFile("appsettings.Development.json", optional: true)
+    .Build();
+
+string username =
+    config["Untis:Username"] ?? throw new InvalidOperationException("Bad appsettings");
+string password =
+    config["Untis:Password"] ?? throw new InvalidOperationException("Bad appsettings");
 
 UntisClient client = new();
-await client.AuthenticateAsync("Linus-Yury.Schneeberg", "6)-f@2E4)$Vwi%Q");
+await client.AuthenticateAsync(username, password);
+Console.WriteLine($"Name: {client.Student.DisplayName}; Class: {client.Classes[0].DisplayName}");
--- UntisAPI/UntisClient.cs
+++ UntisAPI/UntisClient.cs
@@ -1,4 +1,7 @@
 ﻿using System.Net;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using UntisAPI.Json;
 using UntisAPI.ResourceTypes;
 
 namespace UntisAPI;
@@ -6,13 +9,50 @@ namespace UntisAPI;
 public class UntisClient : IDisposable
 {
     public bool Authenticated => _token is not null;
-    public Student Student;
-    public Class Class;
+    public Student Student
+    {
+        get
+        {
+            if (!Authenticated)
+            {
+                throw new InvalidOperationException("Client not authenticated.");
+            }
+            if (_student is null)
+            {
+                throw new InvalidOperationException(
+                    "Student was null (this shouldn't ever happen)"
+                );
+            }
+            return _student;
+        }
+        private set => _student = value;
+    }
+    public List<Class> Classes
+    {
+        get
+        {
+            if (!Authenticated)
+            {
+                throw new InvalidOperationException("Client not authenticated.");
+            }
+            if (_classes is null)
+            {
+                throw new InvalidOperationException(
+                    "Classes was null (this shouldn't ever happen)"
+                );
+            }
+            return _classes;
+        }
+        private set => _classes = value;
+    }
 
     private readonly HttpClient _client;
     private readonly HttpClientHandler _handler;
     private readonly string _apiUrl = "https://hhgym.webuntis.com/WebUntis";
     private string? _token;
+
+    private Student? _student;
+    private List<Class>? _classes;
 
     public UntisClient()
     {
@@ -49,23 +89,38 @@ public class UntisClient : IDisposable
 
         // retrieve token
         HttpResponseMessage tokenResponse = await _client.GetAsync($"{_apiUrl}/api/token/new");
-        foreach (Cookie c in _handler.CookieContainer.GetAllCookies())
-        {
-            Console.WriteLine($"{c.Name}: {c.Value}");
-        }
         tokenResponse.EnsureSuccessStatusCode();
         _token = await tokenResponse.Content.ReadAsStringAsync();
+
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+            "Bearer",
+            _token
+        );
+
+        await fetchIdentity();
     }
 
     private async Task fetchIdentity()
     {
         HttpResponseMessage identityResponse = await _client.GetAsync(
-            "{_apiUrl}/api/rest/view/v1/timetable/filter?resourceType=STUDENT"
+            $"{_apiUrl}/api/rest/view/v1/timetable/filter?resourceType=STUDENT"
         );
-        // TODO:
+        identityResponse.EnsureSuccessStatusCode();
+
+        string json = await identityResponse.Content.ReadAsStringAsync();
+
+        JsonSerializerSettings settings = new() { Converters = [new IdentityConverter()] };
+        Identity identity =
+            JsonConvert.DeserializeObject<Identity>(json, settings)
+            ?? throw new InvalidDataException(
+                "Found unexpected json object while fetching identity"
+            );
+
+        _student = identity.Student;
+        _classes = identity.Classes;
     }
 
-    public async Task<List<List<Entry>>> GetTimetableAsync(DateTime start, DateTime end)
+    public async Task<TimeTable> GetTimetableAsync(DateTime start, DateTime end)
     {
         if (!Authenticated)
         {
@@ -78,6 +133,7 @@ public class UntisClient : IDisposable
             $"{_apiUrl}/api/rest/view/v1/timetable/entries?start={start.Date:yyyy-MM-dd}&end={end.Date:yyyy-MM-dd}&resourceType=STUDENT&resources={Student.Id}"
         );
         // TODO:
+        throw new NotImplementedException();
     }
 
     public void Dispose()
